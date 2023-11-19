39253f2c1953b4fdf39795c6cb2b4e04d0094ec3
lixxkook00 <linhlinhkook00@gmail.com>
2023-11-19 11:44:34 +0700

    first commit

---
+++ src/layouts/SidebarLayout/Sidebar/SidebarMenu/index.tsx
@@ -0,0 +1,255 @@
+import { useContext } from 'react';
+
+import {
+  ListSubheader,
+  alpha,
+  Box,
+  List,
+  styled,
+  Button,
+  ListItem
+} from '@mui/material';
+import { NavLink as RouterLink } from 'react-router-dom';
+import { SidebarContext } from 'src/contexts/SidebarContext';
+
+import DesignServicesTwoToneIcon from '@mui/icons-material/DesignServicesTwoTone';
+import BrightnessLowTwoToneIcon from '@mui/icons-material/BrightnessLowTwoTone';
+import MmsTwoToneIcon from '@mui/icons-material/MmsTwoTone';
+import TableChartTwoToneIcon from '@mui/icons-material/TableChartTwoTone';
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+import BallotTwoToneIcon from '@mui/icons-material/BallotTwoTone';
+import BeachAccessTwoToneIcon from '@mui/icons-material/BeachAccessTwoTone';
+import EmojiEventsTwoToneIcon from '@mui/icons-material/EmojiEventsTwoTone';
+import FilterVintageTwoToneIcon from '@mui/icons-material/FilterVintageTwoTone';
+import HowToVoteTwoToneIcon from '@mui/icons-material/HowToVoteTwoTone';
+import LocalPharmacyTwoToneIcon from '@mui/icons-material/LocalPharmacyTwoTone';
+import RedeemTwoToneIcon from '@mui/icons-material/RedeemTwoTone';
+import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone';
+import TrafficTwoToneIcon from '@mui/icons-material/TrafficTwoTone';
+import CheckBoxTwoToneIcon from '@mui/icons-material/CheckBoxTwoTone';
+import ChromeReaderModeTwoToneIcon from '@mui/icons-material/ChromeReaderModeTwoTone';
+import WorkspacePremiumTwoToneIcon from '@mui/icons-material/WorkspacePremiumTwoTone';
+import CameraFrontTwoToneIcon from '@mui/icons-material/CameraFrontTwoTone';
+import DisplaySettingsTwoToneIcon from '@mui/icons-material/DisplaySettingsTwoTone';
+
+const MenuWrapper = styled(Box)(
+  ({ theme }) => `
+  .MuiList-root {
+    padding: ${theme.spacing(1)};
+
+    & > .MuiList-root {
+      padding: 0 ${theme.spacing(0)} ${theme.spacing(1)};
+    }
+  }
+
+    .MuiListSubheader-root {
+      text-transform: uppercase;
+      font-weight: bold;
+      font-size: ${theme.typography.pxToRem(12)};
+      color: ${theme.colors.alpha.trueWhite[50]};
+      padding: ${theme.spacing(0, 2.5)};
+      line-height: 1.4;
+    }
+`
+);
+
+const SubMenuWrapper = styled(Box)(
+  ({ theme }) => `
+    .MuiList-root {
+
+      .MuiListItem-root {
+        padding: 1px 0;
+
+        .MuiBadge-root {
+          position: absolute;
+          right: ${theme.spacing(3.2)};
+
+          .MuiBadge-standard {
+            background: ${theme.colors.primary.main};
+            font-size: ${theme.typography.pxToRem(10)};
+            font-weight: bold;
+            text-transform: uppercase;
+            color: ${theme.palette.primary.contrastText};
+          }
+        }
+    
+        .MuiButton-root {
+          display: flex;
+          color: ${theme.colors.alpha.trueWhite[70]};
+          background-color: transparent;
+          width: 100%;
+          justify-content: flex-start;
+          padding: ${theme.spacing(1.2, 3)};
+
+          .MuiButton-startIcon,
+          .MuiButton-endIcon {
+            transition: ${theme.transitions.create(['color'])};
+
+            .MuiSvgIcon-root {
+              font-size: inherit;
+              transition: none;
+            }
+          }
+
+          .MuiButton-startIcon {
+            color: ${theme.colors.alpha.trueWhite[30]};
+            font-size: ${theme.typography.pxToRem(20)};
+            margin-right: ${theme.spacing(1)};
+          }
+          
+          .MuiButton-endIcon {
+            color: ${theme.colors.alpha.trueWhite[50]};
+            margin-left: auto;
+            opacity: .8;
+            font-size: ${theme.typography.pxToRem(20)};
+          }
+
+          &.active,
+          &:hover {
+            background-color: ${alpha(theme.colors.alpha.trueWhite[100], 0.06)};
+            color: ${theme.colors.alpha.trueWhite[100]};
+
+            .MuiButton-startIcon,
+            .MuiButton-endIcon {
+              color: ${theme.colors.alpha.trueWhite[100]};
+            }
+          }
+        }
+
+        &.Mui-children {
+          flex-direction: column;
+
+          .MuiBadge-root {
+            position: absolute;
+            right: ${theme.spacing(7)};
+          }
+        }
+
+        .MuiCollapse-root {
+          width: 100%;
+
+          .MuiList-root {
+            padding: ${theme.spacing(1, 0)};
+          }
+
+          .MuiListItem-root {
+            padding: 1px 0;
+
+            .MuiButton-root {
+              padding: ${theme.spacing(0.8, 3)};
+
+              .MuiBadge-root {
+                right: ${theme.spacing(3.2)};
+              }
+
+              &:before {
+                content: ' ';
+                background: ${theme.colors.alpha.trueWhite[100]};
+                opacity: 0;
+                transition: ${theme.transitions.create([
+                  'transform',
+                  'opacity'
+                ])};
+                width: 6px;
+                height: 6px;
+                transform: scale(0);
+                transform-origin: center;
+                border-radius: 20px;
+                margin-right: ${theme.spacing(1.8)};
+              }
+
+              &.active,
+              &:hover {
+
+                &:before {
+                  transform: scale(1);
+                  opacity: 1;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+`
+);
+
+const menuItems = [
+  {
+    subheader: 'Dashboards',
+    items: [
+      { to: '/', icon: <BrightnessLowTwoToneIcon />, text: 'Dashboard' },
+      { to: '/messenger', icon: <MmsTwoToneIcon />, text: 'Messenger' },
+    ],
+  },
+  {
+    subheader: 'Management',
+    items: [
+      { to: '/management/transactions', icon: <TableChartTwoToneIcon />, text: 'Transactions List' },
+    ],
+  },
+  {
+    subheader: 'Accounts',
+    items: [
+      { to: '/management/profile/details', icon: <AccountCircleTwoToneIcon />, text: 'User Profile' },
+      { to: '/management/profile/settings', icon: <DisplaySettingsTwoToneIcon />, text: 'Account Settings' },
+    ],
+  },
+  {
+    subheader: 'Components',
+    items: [
+      { to: '/components/buttons', icon: <HowToVoteTwoToneIcon />, text: 'Buttons' },
+      { to: '/components/modals', icon: <LocalPharmacyTwoToneIcon />, text: 'Modals' },
+      { to: '/components/accordions', icon: <EmojiEventsTwoToneIcon />, text: 'Accordions' },
+      { to: '/components/tabs', icon: <FilterVintageTwoToneIcon />, text: 'Tabs' },
+      { to: '/components/badges', icon: <HowToVoteTwoToneIcon />, text: 'Badges' },
+      { to: '/components/tooltips', icon: <LocalPharmacyTwoToneIcon />, text: 'Tooltips' },
+      { to: '/components/avatars', icon: <RedeemTwoToneIcon />, text: 'Avatars' },
+      { to: '/components/cards', icon: <SettingsTwoToneIcon />, text: 'Cards' },
+      { to: '/components/forms', icon: <TrafficTwoToneIcon />, text: 'Forms' },
+    ],
+  },
+  {
+    subheader: 'Extra Pages',
+    items: [
+      { to: '/status/404', icon: <CheckBoxTwoToneIcon />, text: 'Error 404' },
+      { to: '/status/500', icon: <CameraFrontTwoToneIcon />, text: 'Error 500' },
+      { to: '/status/coming-soon', icon: <ChromeReaderModeTwoToneIcon />, text: 'Coming Soon' },
+      { to: '/status/maintenance', icon: <WorkspacePremiumTwoToneIcon />, text: 'Maintenance' },
+    ],
+  },
+];
+
+function SidebarMenu() {
+  const { closeSidebar } = useContext(SidebarContext);
+
+  return (
+    <div>
+    {menuItems.map((section, index) => (
+      <MenuWrapper key={index}>
+        <List component="div" subheader={<ListSubheader component="div" disableSticky>{section.subheader}</ListSubheader>}>
+          <SubMenuWrapper>
+          <List>
+            {section.items.map((item, itemIndex) => (
+              <ListItem key={itemIndex} component="div">
+                <Button
+                  disableRipple
+                  component={RouterLink}
+                  onClick={closeSidebar}
+                  to={item.to}
+                  startIcon={item.icon}
+                >
+                  {item.text}
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+          </SubMenuWrapper>
+        </List>
+      </MenuWrapper>
+    ))}
+  </div>
+  );
+}
+
+export default SidebarMenu;
